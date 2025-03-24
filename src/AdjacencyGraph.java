edf9f129bfbf65d3ae58116f3afbd52a6e7e2e8d
Benjamin Metzger (RIT Student) <brm2167@rit.edu>
2025-03-24 12:19:54 +0100

    Create the AdjacencyGraph class

---
+++ src/AdjacencyGraph.java
@@ -0,0 +1,46 @@
+/*
+@ASSESSME.USERID: brm2167
+@ASSESSME.AUTHOR: Benjamin R. Metzger
+@ASSESSME.DESCRIPTION: CLASSACTIVITY
+@ASSESSME.ANALYZE: YES
+*/
+
+public class AdjacencyGraph<E> implements Graph<E> {
+
+    @Override
+    public void add(E value) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public boolean contains(E value) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'contains'");
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'size'");
+    }
+
+    @Override
+    public void connectDirected(E a, E b) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'connectDirected'");
+    }
+
+    @Override
+    public void connectUndirected(E a, E b) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'connectUndirected'");
+    }
+
+    @Override
+    public boolean connected(E a, E b) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'connected'");
+    }
+    
+}
