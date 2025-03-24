cef97e08a25870c62b8ce6b3a60283ea13581b79
Benjamin Metzger (RIT Student) <brm2167@rit.edu>
2025-03-24 12:05:13 +0100

    Create the Vertex class

---
+++ src/Vertex.java
@@ -0,0 +1,23 @@
+/*
+@ASSESSME.USERID: brm2167
+@ASSESSME.AUTHOR: Benjamin R. Metzger
+@ASSESSME.DESCRIPTION: CLASSACTIVITY
+@ASSESSME.ANALYZE: YES
+*/
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Vertex<E> {
+    private E value;
+    private Set<Vertex<E>> neighbors;
+
+    /**
+     * Creates a new vertex in a graph.
+     * @param value the value to be stored in the vertex
+     */
+    public Vertex(E value) {
+        this.value = value;
+        neighbors = new HashSet<>();
+    }
+}
