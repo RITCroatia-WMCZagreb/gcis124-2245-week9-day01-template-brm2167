c17524d48d81c9b219013e90cb4a66e134abb42b
Benjamin Metzger (RIT Student) <brm2167@rit.edu>
2025-03-24 11:46:23 +0100

    Create the Graph interface

--- src/Graph.java
+++ src/Graph.java
@@ -5,6 +5,11 @@
 @ASSESSME.ANALYZE: YES
 */
 
-public interface Graph {
-    
+public interface Graph<E> {
+    void add(E value);
+    boolean contains(E value);
+    int size();
+    void connectDirected(E a, E b);
+    void connectUndirected(E a, E b);
+    boolean connected(E a, E b);
 }
